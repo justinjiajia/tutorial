3d8c87a57192128ac927adfc0bbca91760ac234c
Justin Jia <jiajia1015@gmail.com>
2024-08-22 17:39:54 +0800

    Update create_dynamic_chart.py

--- widgets/create_dynamic_chart.py
+++ widgets/create_dynamic_chart.py
@@ -14,13 +14,19 @@ st.dataframe(source)
 
 with st.container(border=True):
     stocks = st.multiselect("Select stocks for comparison", source.symbol.unique(), ['AMZN', 'AAPL'])
+    all_years = st.checkbox("Show prices for the whole period")
+    year = st.slider("Select a year", 2005, 2009, disabled=all_years)
     
-    all = st.checkbox("Show prices for the whole period")
-    year = st.slider("Select a year", 2005, 2009, disabled=all)
-    st.markdown(f"### Stock prices in {year}<br>", unsafe_allow_html=True)
-    if not all:
+ 
+    if not all_years:
         chart_data = source.query(f"date < {year + 1} and date >= {year} and symbol in {stocks}")
     else:
         chart_data = source.query(f"symbol in {stocks}")  
-    st.line_chart(chart_data, x="date", y="price", color="symbol", width=600, height=500, use_container_width=False)
+        year = "2005-2009"
+    if not stocks:
+        st.error("Please select at least one stock!")
+    else:
+        st.markdown(f"### Stock prices in {year}<br>", unsafe_allow_html=True)
+        st.line_chart(chart_data, x="date", y="price", color="symbol", 
+                      width=600, height=500, use_container_width=False)
 
