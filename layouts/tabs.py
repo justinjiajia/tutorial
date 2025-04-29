3b56af534dd5db1478e12d41d2f6ef0daf72e726
justinjiajia <jiajia1015@gmail.com>
2025-04-30 01:54:50 +0800

    update

--- layouts/tabs.py
+++ layouts/tabs.py
@@ -15,10 +15,10 @@ st.markdown("""`st.container` allows us to inserts an invisible container into o
 
 
 
-st.markdown("""Calling the `st.columns()` function returns a list of column objects.
+st.markdown("""Calling the `st.tabs()` function returns a list of tab objects.
 
 
-To add elements to the returned container, we can use the `with` statement (preferred) or just call methods directly on the returned object..""")
+To add elements to the returned container, we can use the `with` statement (preferred) or just call funcions directly on the returned object.""")
 
 
 
