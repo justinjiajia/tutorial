6a09c17942f0fbaac81bef64c69afb2c5d716fa8
justinjiajia <jiajia1015@gmail.com>
2025-05-27 01:34:03 +0800

    update

--- dashboard/dashboard_1.py
+++ dashboard/dashboard_1.py
@@ -1,10 +1,14 @@
 import streamlit as st
 import pandas as pd
 import yfinance as yf
+from datetime import datetime, date
+import sys
 
-ticker = yf.Ticker("AAPL")
- 
+sys.path.append('..')
 
+from utils import load_to_df
+
+# ticker = yf.Ticker("AAPL")
 
 # ticker.info.get('longName')
 # ticker.info.get('sector')
@@ -17,40 +21,91 @@ symbol_list = ['AAPL', 'ABBV', 'ABT', 'ACN', 'ADBE', 'AIG', 'AMD', 'AMGN', 'AMT'
                'UNH', 'UNP', 'UPS', 'USB', 'V', 'VZ', 'WFC', 'WMT', 'XOM']
 
 
+# name_list = []
+# sector_list = []
+# market_cap_list = []
+ 
+# for ticker in symbol_list:
+#    stock = yf.Ticker(ticker)
+#    info = stock.info
 
-name_list = []
-sector_list = []
-market_cap_list = []
+#    name_list.append(info.get('longName'))
+#    sector_list.append(info.get('sector'))
+#    market_cap = info.get('marketCap')
+#    market_cap_bn = round(market_cap / 1e9, 2)
+#    market_cap_list.append(market_cap_bn)
 
-# Fill in the code below
-for ticker in symbol_list:
-    stock = yf.Ticker(ticker)
-    info = stock.info
+# ticker_info_df = pd.DataFrame({'Ticker': symbol_list, 'Company':name_list, 'Sector':sector_list, 'Market Cap (B)': market_cap_list})
+# ticker_info_df.to_csv('ticker_info.csv', index=False)   
 
-    name_list.append(info.get('longName'))
-    sector_list.append(info.get('sector'))
-    market_cap = info.get('marketCap')
-    market_cap_bn = round(market_cap / 1e9, 2)
-    market_cap_list.append(market_cap_bn)
+url = "https://raw.githubusercontent.com/justinjiajia/datafiles/refs/heads/main/ticker_info.csv"
 
+ticker_info = load_to_df(url)
+ 
+@st.cache_data(show_spinner=False)
+def yf_download(symbol_list, start_date="2020-01-01", end_date=f"{date.today().year}-12-31"):
+    return yf.download(symbol_list, start=start_date, end=end_date)
 
-ticker_info_df = pd.DataFrame({'Ticker': symbol_list, 'Company':name_list, 'Sector':sector_list, 'Market Cap (B)': market_cap_list})
-ticker_info_df.to_csv('ticker_info.csv')    
-
-
-start_date = "2020-01-01"
-end_date = "2025-05-06"  # Update to today's date or use date.today()
 
 # Download OHLC data (Open, High, Low, Close, Volume, etc.)
-stock_data = yf.download(
-    symbol_list,
-    start=start_date,
-    end=end_date,
-)
+stock_data = yf_download(symbol_list)
+
 
 # This transformation converts stock data from a wide format (where each ticker is a column) into a long format (where each ticker's data is in separate rows).
 long_stock_data = (stock_data.stack(level='Ticker')  # Stack the ticker level; stock_data has a MultiIndex (hierarchical index) where one of the levels is 'Ticker'. stack() pivots the specified level ('Ticker') from columns into rows, making the DataFrame longer (more rows, fewer columns).
              .reset_index()        # After stacking, the DataFrame has a new index structure. reset_index() converts all index levels into regular columns, making it easier to work with the data.
              .rename(columns={'level_1': 'Ticker'}))  # When stacking and resetting, the stacked column might get a default name like 'level_1'. This renames that column back to 'Ticker' for clarity.
 
-st.write(long_stock_data)
+
+# extract S&P 100 tickers from ticker_info
+tickers_100 = ticker_info.Ticker.unique()
+
+# set page title
+st.title("S&P 100 Stock Dashboard 📊")  
+
+
+
+# Sidebar controls
+with st.sidebar:
+
+    show_sector = st.checkbox("Show Market Cap by Sector", True)
+
+    selected_tickers = st.multiselect(
+        "Select Companies",
+        options=tickers_100,
+        default=['TSLA', 'NVDA', 'AAPL']
+    )
+
+    # Year range slider
+    selected_years = st.slider(
+        "Select Year Range",
+        min_value=2020,
+        max_value=2025,
+        value=(2024, 2025)
+    )
+
+if show_sector:
+    st.header("Market Cap by Sector")
+    st.bar_chart(ticker_info, x="Sector", y="Market Cap (B)", color="Ticker", horizontal=True,
+            width=720, height=500)
+
+if selected_tickers:
+
+    chart_data = long_stock_data.query(f"Date <= '{selected_years[1]}-12-31' and Date >= '{selected_years[0]}-01-01' and Ticker in {selected_tickers}")
+
+
+    st.header(f"Stock Trend Analysis ({selected_years[0]} - {selected_years[1]})")
+    
+    # Closing Prices
+    st.subheader("Closing Prices")
+    st.line_chart(chart_data, x="Date", y="Close", color="Ticker",
+                  width=720, height=500)
+
+    
+    # Volume Analysis
+    st.subheader("Trading Volume")
+    st.bar_chart(chart_data, x="Date", y="Volume", color="Ticker", 
+                  width=720, height=500)
+
+else:
+    st.error("Please select at least one stock")
